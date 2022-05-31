399f2368f2c2659d41b985197caafd11ea0f41c4
satanxxzero <slavadorgamboa09@gmail.com>
2022-05-31 02:35:40 -0600

    primer commit

---
+++ src/components/Scroll.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Image, ScrollView, Text } from 'react-native';
+
+const img = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64
+};
+
+const App = () => (
+  <ScrollView>
+    <Image source={img} />
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+    <Text style={{ fontSize: 20 }}>Este es un texto muy largo para ver</Text>
+  </ScrollView>
+);
+
+export default App;
